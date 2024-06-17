e4f606f74bed8333e544ad8080c633eaab93d349
longlodw <longlodw@my.yorku.ca>
2024-06-17 04:13:54 -0400

    use correct cpu and memory usage

--- include/monitor.hpp
+++ include/monitor.hpp
@@ -1,7 +1,10 @@
 #pragma once
+#include <cstddef>
+#include <utility>
 
-namespace monitor
-{
-  float get_cpu_usage();
-  float get_memory_usage();
+namespace monitor {
+  int sample_cpu_usage(std::pair<size_t, size_t> &s);
+  int sample_memory_usage(std::pair<size_t, size_t> &s);
+  float get_cpu_usage(size_t us);
+  float get_memory_usage(size_t us);
 }
--- src/monitor.cpp
+++ src/monitor.cpp
@@ -1,5 +1,6 @@
 #include "../include/monitor.hpp"
 #include <cstdio>
+#include <unistd.h>
 
 class fp_closer {
 public:
@@ -11,26 +12,57 @@ private:
   FILE* file;
 };
 
-float monitor::get_cpu_usage() {
+float monitor::get_cpu_usage(size_t us) {
+  std::pair<size_t, size_t> s1, s2;
+  if (sample_cpu_usage(s1) != 0) {
+    return -1;
+  }
+  usleep(us);
+  if (sample_cpu_usage(s2) != 0) {
+    return -1;
+  }
+  size_t total = s2.first - s1.first;
+  if (total == 0) {
+    return 0;
+  }
+  size_t idle = s2.second - s1.second;
+  return 1.0 - (float)idle / total;
+}
+
+float monitor::get_memory_usage(size_t us) {
+  std::pair<size_t, size_t> s1, s2;
+  if (sample_memory_usage(s1) != 0) {
+    return -1;
+  }
+  usleep(ms);
+  if (sample_memory_usage(s2) != 0) {
+    return -1;
+  }
+  size_t total = s2.first - s1.first;
+  if (total == 0) {
+    return 0;
+  }
+  size_t idle = s2.second - s1.second;
+  return 1.0 - (float)idle / total;
+}
+
+int monitor::sample_cpu_usage(std::pair<size_t, size_t> &s) {
   FILE* file = fopen("/proc/stat", "r");
   if (file == NULL) {
     return -1;
   }
   fp_closer closer(file);
 
-  unsigned long long user = 0, nice = 0, system = 0, idle = 0, iowait = 0, irq = 0, softirq = 0, steal = 0, guest = 0, guest_nice = 0;
+  size_t user = 0, nice = 0, system = 0, idle = 0, iowait = 0, irq = 0, softirq = 0, steal = 0, guest = 0, guest_nice = 0;
   if (10 != fscanf(file, "cpu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", &user, &nice, &system, &idle, &iowait, &irq, &softirq, &steal, &guest, &guest_nice)) {
     return -2;
   }
-  unsigned long long total = user + nice + system + idle + iowait + irq + softirq + steal;
-  if (total == 0) {
-    return 0.0;
-  }
-
-  return ((double)(total - idle)) / total;
+  size_t total = user + nice + system + idle + iowait + irq + softirq + steal;
+  s = std::make_pair(total - idle, total);
+  return 0;
 }
 
-float monitor::get_memory_usage() {
+int monitor::sample_memory_usage(std::pair<size_t, size_t> &s) {
   FILE* file = fopen("/proc/meminfo", "r");
   if (file == NULL) {
     return -1;
@@ -41,10 +73,7 @@ float monitor::get_memory_usage() {
   if(3 != fscanf(file, "MemTotal: %llu kB\nMemFree: %llu kB\nMemAvailable: %llu kB", &total, &free, &available)) {
     return -2;
   }
-  if (total == 0) {
-    return 0.0;
-  }
   double used = total - available;
-
-  return used / total;
+  s = std::make_pair(used, total);
+  return 0;
 }
